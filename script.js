53ec3d7adbd5cd31d803e5e5e22412c0ba8f8ddd
PintilieGeani <pintilie.geany@yahoo.it>
2025-08-19 19:40:21 +0200

    feat: bonus 1

--- script.js
+++ script.js
@@ -25,6 +25,56 @@
 // Stampare i dati in console in un messaggio ben formattato.
 // Testa la funzione con la query "london"
 
+// const apiUrl = "http://localhost:3333"
+
+// async function fetchUrl(url) {
+//     const response = await fetch(url)
+//     const obj = await response.json()
+
+//     return obj
+// }
+
+
+
+// async function getDashboardData(query) {
+//     try{
+//         // Chiamate api
+//         const destinationPromise = fetchUrl(`${apiUrl}/destinations?search=${query}`)
+//         const weatherPromise =  fetchUrl(`${apiUrl}/weathers?search=${query}`)
+//         const airportPromise = fetchUrl(`${apiUrl}/airports?search=${query}`)
+
+//         // Array di primises
+//         const promises = [destinationPromise, weatherPromise, airportPromise]
+
+//         // Destruturazione
+//         const [destination, weather, airport] = await Promise.all(promises)
+
+//         return {
+//             city: destination[0].name,
+//             country: destination[0].country,
+//             temperature: weather[0].temperature,
+//             weather: weather[0].weather_description,
+//             airport: airport[0].name
+//         }
+//     }
+//     catch(error){
+//         throw new Error("Errore nel recupero dei dati del api")
+//     }
+
+// }
+
+// const dashboard = getDashboardData("Paris")
+// .then(dashboard =>{
+//     console.log("I dati estrtti sono:", dashboard)
+//     console.log(`${dashboard.city} is in ${dashboard.country}. Today there are ${dashboard.temperature} degrees and the weather is ${dashboard.weather}. The mai airport is ${dashboard.airport}`)
+// })
+// .catch((error) => console.error(error))
+
+
+// 🎯 Bonus 1 - Risultato vuoto
+// Se l’array di ricerca è vuoto, invece di far fallire l'intera funzione, semplicemente i dati relativi a quella chiamata verranno settati a null e la frase relativa non viene stampata. Testa la funzione con la query “vienna” (non trova il meteo).
+
+
 const apiUrl = "http://localhost:3333"
 
 async function fetchUrl(url) {
@@ -37,37 +87,43 @@ async function fetchUrl(url) {
 
 
 async function getDashboardData(query) {
-    try{
+    try {
         // Chiamate api
         const destinationPromise = fetchUrl(`${apiUrl}/destinations?search=${query}`)
-        const weatherPromise =  fetchUrl(`${apiUrl}/weathers?search=${query}`)
+        const weatherPromise = fetchUrl(`${apiUrl}/weathers?search=${query}`)
         const airportPromise = fetchUrl(`${apiUrl}/airports?search=${query}`)
-    
+
         // Array di primises
         const promises = [destinationPromise, weatherPromise, airportPromise]
-    
+
         // Destruturazione
         const [destination, weather, airport] = await Promise.all(promises)
-    
+
         return {
-            city: destination[0].name,
-            country: destination[0].country,
-            temperature: weather[0].temperature,
-            weather: weather[0].weather_description,
-            airport: airport[0].name
+            city: (destination.length === 0 ? null : destination[0].name),
+            country: (destination.length === 0 ? null : destination[0].country),
+            temperature: (weather.length === 0 ? null : weather[0].temperature),
+            weather: (weather.length === 0 ? null : weather[0].weather_description),
+            airport: (airport.length === 0 ? null : airport[0].name)
         }
     }
-    catch(error){
+    catch (error) {
         throw new Error("Errore nel recupero dei dati del api")
     }
 
 }
 
-const dashboard = getDashboardData("Paris")
-.then(dashboard =>{
-    console.log("I dati estrtti sono:", dashboard)
-    console.log(`${dashboard.city} is in ${dashboard.country}. Today there are ${dashboard.temperature} degrees and the weather is ${dashboard.weather}. The mai airport is ${dashboard.airport}`)
-})
-.catch((error) => console.error(error))
-
-
+const dashboard = getDashboardData("london")
+    .then(dashboard => {
+        console.log("I dati estrtti sono:", dashboard)
+        console.log(`${dashboard.city == null || dashboard.country == null
+            ? ""
+            : `${dashboard.city} is in ${dashboard.country}.`}
+        ${dashboard.weather == null
+                ? ""
+                : `Today there are ${dashboard.temperature} degrees and the weather is ${dashboard.weather}`} 
+        The main airport is ${dashboard.airport == null
+                ? ""
+                : dashboard.airport}`)
+    })
+    .catch((error) => console.error(error))
